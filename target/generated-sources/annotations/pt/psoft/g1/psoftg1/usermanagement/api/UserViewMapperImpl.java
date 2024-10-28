4bbe04d8b801848463240dcccafe08ae5521511c
RitaPinto1230205 <123025@isep.ipp.pt>
2024-10-28 01:25:44 +0000

    Added Surefire plugin to filter my tags

--- target/generated-sources/annotations/pt/psoft/g1/psoftg1/usermanagement/api/UserViewMapperImpl.java
+++ target/generated-sources/annotations/pt/psoft/g1/psoftg1/usermanagement/api/UserViewMapperImpl.java
@@ -9,7 +9,7 @@ import pt.psoft.g1.psoftg1.usermanagement.model.User;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-10-28T00:54:22+0000",
+    date = "2024-10-28T01:23:54+0000",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Amazon.com Inc.)"
 )
 @Component
