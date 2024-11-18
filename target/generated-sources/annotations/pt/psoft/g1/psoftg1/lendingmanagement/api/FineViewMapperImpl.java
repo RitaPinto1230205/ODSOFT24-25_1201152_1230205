1ac0cfb8a54e98bbe87b78a2ddc5b9ffe5bcb3a3
RitaPinto1230205 <123025@isep.ipp.pt>
2024-11-18 02:08:05 +0000

    Improvement Pt1- get the pipeline working - added different jacoco reports to my different tests

--- target/generated-sources/annotations/pt/psoft/g1/psoftg1/lendingmanagement/api/FineViewMapperImpl.java
+++ target/generated-sources/annotations/pt/psoft/g1/psoftg1/lendingmanagement/api/FineViewMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-11-18T01:57:07+0000",
+    date = "2024-11-18T02:04:31+0000",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Amazon.com Inc.)"
 )
 @Component
--- target/generated-sources/annotations/pt/psoft/g1/psoftg1/usermanagement/services/EditUserMapperImpl.java
+++ target/generated-sources/annotations/pt/psoft/g1/psoftg1/usermanagement/services/EditUserMapperImpl.java
@@ -8,7 +8,7 @@ import pt.psoft.g1.psoftg1.usermanagement.model.User;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-11-18T01:57:07+0000",
+    date = "2024-11-18T02:04:31+0000",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Amazon.com Inc.)"
 )
 @Component
